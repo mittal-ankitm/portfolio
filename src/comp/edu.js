48e24556812bb17d0538de35e3fda562199f1a4e
Ankit Mittal <mittal.ankitm@gmail.com>
2021-01-30 20:23:16 +0530

    updated

--- src/comp/edu.js
+++ src/comp/edu.js
@@ -12,9 +12,9 @@ return(
     <div className='expgroup col-md-8 col-lg-8 col-xl-8'>
   <div class="educard">
     <h5 class="">Indian Institute of Information Technology and Management, Gwalior</h5>
-    <div class="">B.Tech in CSE</div>
-    <div> CGPA 8.02</div>
+    <div class="">B.Tech in Computer Science</div>
     <div>2018-2022</div>
+    <div> CGPA 8.02</div>
   </div>
 </div>
 </div>
