823f847867b1065b492272a8b2dc41146faf79c0
Ankit Mittal <mittal.ankitm@gmail.com>
2021-02-09 18:01:50 +0530

    publ

--- src/comp/publication.js
+++ src/comp/publication.js
@@ -1,19 +1,19 @@
 import React,{useEffect,useState,useContext} from 'react'
 
 
-const Connect= (props)=>{
+const Publication= (props)=>{
 
 return(
 <div style={{padding:'2%'}}>
 <h2 style={{padding:'1%'}}>Publications</h2>
 
     
-    <ul>
-    <li class="card-text" style={{listStylePosition:'outside'}}>Ankit Mittal, Bhavyansh Sharma and Pinku Ranjan “Real Estate Management System based on Blockchain” 2020 7th IEEE Uttar Pradesh Section International Conference on Electrical, Electronics and Computer Engineering (UPCON), Allahabad, India, 2020
-   </li></ul>
+    <div className='card'>
+    <div class="card-body">Ankit Mittal, Bhavyansh Sharma and Pinku Ranjan “Real Estate Management System based on Blockchain” 2020 7th IEEE Uttar Pradesh Section International Conference on Electrical, Electronics and Computer Engineering (UPCON), Allahabad, India, 2020
+   </div></div>
   </div>
 )
 
 }
 
-export default Connect
+export default Publication
