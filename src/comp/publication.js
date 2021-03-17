eb31fdda9daac5e3a35313578925a3cb02946dd1
Ankit Mittal <mittal.ankitm@gmail.com>
2021-03-17 13:45:56 +0530

    link updated

--- src/comp/publication.js
+++ src/comp/publication.js
@@ -9,8 +9,12 @@ return(
 
     
     <div className='card' style={{width:'90%'}}>
-    <div class="card-body">Ankit Mittal, Bhavyansh Sharma and Pinku Ranjan “Real Estate Management System based on Blockchain” 2020 7th IEEE Uttar Pradesh Section International Conference on Electrical, Electronics and Computer Engineering (UPCON), Allahabad, India, 2020
-   </div></div>
+    <a href='https://ieeexplore.ieee.org/document/9376540' target='_blank' >
+    <div class="card-body">
+    A. Mittal, B. Sharma and P. Ranjan, "Real Estate Management System based on Blockchain," 2020 IEEE 7th Uttar Pradesh Section International Conference on Electrical, Electronics and Computer Engineering (UPCON), Prayagraj, India, 2020, pp. 1-6, doi: 10.1109/UPCON50219.2020.9376540.
+   </div>
+   </a>
+   </div>
   </div>
 )
 
